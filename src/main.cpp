80f0e4d774a4dd4a98dd6114b108c95dc913f396
Nick Hackney <nwhackney@gmail.com>
2020-07-29 19:08:50 -0400

    adf

--- src/main.cpp
+++ src/main.cpp
@@ -105,12 +105,12 @@ void run_config()
 		Temp=1.0/cosh(w*slope*((double) t));
 		crystal.Metropolis(Temp,Edat,accepted);
 
-		// if (t%100000==0)
-		// {
-		// 	stringstream look;
-		// 	look<<"snap_"<<<<t;
-		// 	crystal.print_gnu(look.str());
-		// }
+		if (t%100000==0)
+		{
+			stringstream look;
+			look<<"snap_"<<<<t;
+			crystal.print_gnu(look.str());
+		}
 
 		if (t%1000==0)
 		{
